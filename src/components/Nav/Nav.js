d44a1fb262ca4e0c68d549c89449c166034d94e3
camilo17 <cami172013@gmail.com>
2018-08-07 17:00:30 -0500

    removed the randomTheme picker

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -56,10 +56,7 @@ class Nav extends Component {
         }}
         style={stickyStyles}
       >
-        <div className="magic-wand bounce-xy" onClick={(e) => switchTheme()}>
-          <button className="fas fa-magic fa-lg" href="#" />
-          <div className="magic-text">Color Me</div>
-        </div>
+        
         <style jsx="true">
           {`
             .menu__item:hover {
