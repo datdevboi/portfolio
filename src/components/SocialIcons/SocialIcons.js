9f778c72e983f15a3e56b4312cb85f31bee6c553
datdevboi <cami172013@gmail.com>
2018-12-03 15:39:42 -0600

    Added Medium icon with a link to my Medium profile

--- src/components/SocialIcons/SocialIcons.js
+++ src/components/SocialIcons/SocialIcons.js
@@ -1,21 +1,40 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
-import './style.scss';
+import "./style.scss";
 
 const SocialIcons = (props, context) => {
-  const { theme: { colorPrimary } } = context;
-  
+  const {
+    theme: { colorPrimary }
+  } = context;
+
   return (
     <div className="social-icons animate-icons">
-        <a target="_blank" rel="noopener noreferrer" href="https://github.com/datdevboi" style={ { color: colorPrimary } }><i className="fab fa-github"></i></a>
-        <a target="_blank" rel="noopener noreferrer" href="https://app.codesignal.com/profile/datdevboi" style={ { color: colorPrimary } }><div className="codesignal-icon"></div></a>
-        
-        
-        
-        
-       
-      </div>
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://github.com/datdevboi"
+        style={{ color: colorPrimary }}
+      >
+        <i className="fab fa-github" />
+      </a>
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://app.codesignal.com/profile/datdevboi"
+        style={{ color: colorPrimary }}
+      >
+        <div className="codesignal-icon" />
+      </a>
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://medium.com/@camiloguerrero"
+        style={{ color: colorPrimary }}
+      >
+        <i className="fab fa-medium" />
+      </a>
+    </div>
   );
 };
 
